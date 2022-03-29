38ec3b53bd318890e85946fbea1516a767c24b81
christianZDH <cristian5732z@outlook.com>
2022-03-29 10:22:02 -0800

    tabs changes

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-
+  tabSelected: string;
   constructor() {}
 
+  onChangeTab(ev: any) {
+    this.tabSelected = ev.tab;
+  }
 }
